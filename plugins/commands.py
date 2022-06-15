6dfe6ed8707e540fc5c3c7434acdc8e2c71c718d
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-06-15 18:19:58 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -42,7 +42,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('🕊️ സിനിമാകവാടം 🕊️', url='https://t.me/+HYF-qD6mwwM3NmJl')
+            InlineKeyboardButton('🕊️ സിനിമാകവാടം 🕊️', url='https://t.me/Cinima_kavadam2022')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
@@ -82,7 +82,7 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('🕊️ സിനിമാകവാടം 🕊️', url='https://t.me/+HYF-qD6mwwM3NmJl')
+            InlineKeyboardButton('🕊️ സിനിമാകവാടം 🕊️', url='https://t.me/Cinima_kavadam2022')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
